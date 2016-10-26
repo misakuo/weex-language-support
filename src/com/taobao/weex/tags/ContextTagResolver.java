dc2ee1b19ec8437ac483cc87f69931aa4d14d99b
moxun.ljf <moxun.ljf@alibaba-inc.com>
2016-10-26 14:11:52 +0800

    fix #5

--- src/com/taobao/weex/tags/ContextTagResolver.java
+++ src/com/taobao/weex/tags/ContextTagResolver.java
@@ -19,13 +19,19 @@ public class ContextTagResolver {
         List<WeexTag> result = new ArrayList<WeexTag>();
         PsiFile file = any.getContainingFile();
         PsiDirectory directory = file.getOriginalFile().getContainingDirectory();
+        if (directory == null) {
+            directory = file.getOriginalFile().getParent();
+        }
+        if (directory == null) {
+            return result;
+        }
         PsiFile[] files = directory.getFiles();
         for (PsiFile p : files) {
             if (!p.getName().equals(any.getContainingFile().getName())) {
                 WeexTag common = DirectiveLint.getCommonTag();
                 if (common != null) {
                     WeexTag fake = new WeexTag();
-                    fake.tag = p.getName().replace(".we","");
+                    fake.tag = p.getName().replace(".we", "");
                     fake.attrs = new CopyOnWriteArrayList<Attribute>(common.attrs);
                     fake.declare = p;
                     result.add(fake);
